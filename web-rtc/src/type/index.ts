dcd1f46b4c2a4b04d9fafe5957b22fe5293e4ddd
장민우 <76735567+Jowen0@users.noreply.github.com>
2023-05-17 15:12:05 +0900

    소켓 연결 관련 오류 수정 및 useClose Hook 추가

--- web-rtc/src/type/index.ts
+++ web-rtc/src/type/index.ts
@@ -1,4 +1,8 @@
+import { CLOSE_CODE } from "../const"
+
 export type Data = {
     type: string,
-    data: string
+    data: any
 } & Record<string, unknown>
+
+export type CloseCode = typeof CLOSE_CODE[keyof typeof CLOSE_CODE]
